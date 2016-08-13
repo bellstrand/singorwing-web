195581313fc75a52a3d1141a837bebfe56f96ba0
Magnus Bellstrand <bellstrand@gmail.com>
2016-08-13 15:41:40 +0200

    bundle hotfix

--- build/export.js
+++ build/export.js
@@ -4,15 +4,7 @@ module.exports = {
 		"config.js",
 		"favicon.ico",
 		"LICENSE.md",
-		"jspm_packages/system.js",
-		"jspm_packages/system-polyfills.js",
-		"jspm_packages/system-csp-production.js",
+		"jspm_packages/**",
 		"dist/style.css",
-		"jspm_packages/npm/font-awesome@*/css/font-awesome.min.css",
-		"jspm_packages/npm/font-awesome@*/fonts/*",
-		"jspm_packages/github/github/fetch@*.js",
-		"jspm_packages/github/github/fetch@*/fetch.js",
-		"jspm_packages/npm/youtube-player@*.js",
-		"jspm_packages/npm/youtube-player@*/dist/*",
 	]
 };
